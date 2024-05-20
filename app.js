276bd6d9fec576d251f0b05693f90014b5359d0d
hassan03042 <muhammadhassan6948@gmail.com>
2024-05-20 21:26:43 +0500

    code uploaded

---
+++ app.js
@@ -0,0 +1,27 @@
+let heading = document.getElementById('heading');
+let text = document.getElementById('text');
+
+function bold(){
+    heading.style.fontWeight='900'
+    text.style.fontWeight='800'
+}
+function Italic(){
+    heading.style.fontStyle='italic'
+    text.style.fontStyle='italic'
+}
+function Underline(){
+    heading.style.textDecoration='underline'
+    text.style.textDecoration='underline'
+}
+function align_left(){
+    heading.style.textAlign='left'
+    text.style.textAlign='left'
+}
+function align_Right(){
+    heading.style.textAlign='right'
+    text.style.textAlign='right'
+}
+function align_center(){
+    heading.style.textAlign='center'
+    text.style.textAlign='center'
+}
